0c0eaacddec381bc98339ac484be1756f9cdd6c2
Sebastian Sala <sebastian_orko@hotmail.com>
2023-07-22 11:50:25 -0600

    encoding resolved

--- components/PathNav.tsx
+++ components/PathNav.tsx
@@ -1,16 +1,31 @@
 "use client"
 
 import Link from "next/link"
-import { useSelectedLayoutSegments } from "next/navigation"
+import { useSelectedLayoutSegments, useRouter } from "next/navigation"
 
 const PathNav = () => {
   const segments = useSelectedLayoutSegments()
-
+  const router = useRouter()
+  const handleClick = (segment: string) => {
+    if (segment === segments.at(-1)) {
+      return
+    }
+    const path = segments.slice(0, segments.indexOf(segment) + 1).join("/")
+    router.push(`/${path}`)
+  }
   return (
-    <nav className='container mx-auto'>
-      <ul className='flex gap-4'>
+    <nav className='container mx-auto mt-3'>
+      <ul className='flex gap-x-6'>
         <Link href='/'>Home</Link>
-        <Link href={segments}>{segments}</Link>
+        {segments.map((segment, index) => (
+          <span
+            className='cursor-pointer'
+            onClick={() => handleClick(segment)}
+            key={index}
+          >
+            {segment}
+          </span>
+        ))}
       </ul>
     </nav>
   )
