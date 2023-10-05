e6546dbcba70857c960410b7811171a947831317
Sebastian Sala <sebastian_orko@hotmail.com>
2023-10-05 13:11:58 -0600

    testing conditions

--- components/filtered-products/filtered-products-list.tsx
+++ components/filtered-products/filtered-products-list.tsx
@@ -6,7 +6,7 @@ import type { ProductType } from '@/types/db'
 
 type FilteredProductsListProps = {
   products: ProductType[]
-  searchParams: Record<string, string | string[]>
+  searchParams?: Record<string, string | string[]>
 }
 
 export default async function FilteredProductsList({
@@ -15,15 +15,15 @@ export default async function FilteredProductsList({
 }: FilteredProductsListProps) {
   console.log(products)
   console.log(searchParams)
-  console.log(Object.keys(searchParams).length)
-
-  const conditions = Object.values(searchParams).map((categoryId) => {
-    return Array.isArray(categoryId)
-      ? categoryId.map(Number)
-      : Number(categoryId)
-  })
-
-  console.log(conditions)
+  if (searchParams) {
+    console.log(Object.keys(searchParams).length)
+    const conditions = Object.values(searchParams).map((categoryId) => {
+      return Array.isArray(categoryId)
+        ? categoryId.map(Number)
+        : Number(categoryId)
+    })
+    console.log(conditions)
+  }
 
   return (
     <ul className="flex flex-wrap justify-center items-center mt-9 pb-7  overflow-x-auto scrollbar-hide sm:grid sm:grid-cols-2 sm:gap-10 md:gap-14 md:overflow-x-hidden lg:grid-cols-4">
