cb9832e16a521d3f235772964fd416e609063a93
sebas-sala <sebastian_orko@hotmail.com>
2024-11-24 13:25:31 -0600

    refactor: comment out CartProvider and related code to improve clarity and reduce unused imports

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,13 @@
-import AuthProvider from "@/contexts/auth-context"
-import CartProvider from "@/contexts/cart-context"
+import AuthProvider from "@/contexts/auth-context";
+// import CartProvider from "@/contexts/cart-context"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AuthProvider>
-        <CartProvider>{children}</CartProvider>
+        {/* <CartProvider>{children}</CartProvider> */}
+        {children}
       </AuthProvider>
     </>
-  )
+  );
 }
